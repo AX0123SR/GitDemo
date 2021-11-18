8d5955776b3c636ec061f5ed0afbaa542e3e7b71
AX0123SR <comeayush2@gmail.com>
2021-11-18 17:35:48 +0530

    latest commit

--- Image_To_ASCII_Art.py
+++ Image_To_ASCII_Art.py
@@ -3,7 +3,7 @@ pywhatkit.image_to_ascii_art("pikachu.jpg","pikachu.txt")
 
 pywhatkit.image_to_ascii_art("doraemon.png","doraemon.txt")
 
-
+# This is an image to ASCII art converter file.
 #############
 #########
 
